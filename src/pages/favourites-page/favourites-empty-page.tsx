11bd00bb8c95ba757bfed38de68afac169a6709d
vulpeAlba <90221448+vulpeAlba@users.noreply.github.com>
2024-02-28 18:29:13 +0300

    module2-task1

--- src/pages/favourites-page/favourites-empty-page.tsx
+++ src/pages/favourites-page/favourites-empty-page.tsx
@@ -6,7 +6,7 @@ function FavouritesEmptyPage(): JSX.Element {
           <div className="header__wrapper">
             <div className="header__left">
               <a className="header__logo-link" href="main.html">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41">
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
               </a>
             </div>
             <nav className="header__nav">
